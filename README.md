36d2ef32b6fab741d6662d3f5415866dd11f0940
Mateus Pinheiro <mateusp@br.ibm.com>
2017-04-07 14:08:49 -0300

    Blank cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_breaking_machine
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'chef_breaking_machine'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures chef_breaking_machine'
+long_description 'Installs/Configures chef_breaking_machine'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/chef_breaking_machine/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/chef_breaking_machine' if respond_to?(:source_url)
